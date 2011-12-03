0caaa7bbceb9b81bc8191d426d2f93a10813933f
David Tchepak <dave@davesquared.net>
2011-12-04 01:55:02 +1100

    Adding count_neighbours function

--- life.hs
+++ life.hs
@@ -10,8 +10,13 @@ is_neighbour (Point x1 y1) (Point x2 y2)
     | otherwise         = False
     where   dx = abs (x1-x2)
             dy = abs (y1-y2)
-            is_same_point = dx == dy
+            is_same_point = x1 == x2 && y1 == y2
 
+count_neighbours :: Point -> [Point] -> Integer
+count_neighbours _ [] = 0
+count_neighbours pt (x:xs) = 
+    let single_neighbour = if is_neighbour pt x then 1 else 0
+    in  single_neighbour + count_neighbours pt xs
 
 assert_equal :: Eq a => Show a => a -> a -> IO()
 assert_equal x y
@@ -23,4 +28,6 @@ main = do
         assert_equal True (is_neighbour (Point 1 1) (Point 1 2))
         assert_equal False (is_neighbour (Point 1 1) (Point 1 1))
         assert_equal False (is_neighbour (Point 321 1) (Point 1 1))
+        assert_equal 0 (count_neighbours (Point 0 0) [(Point 3 4)])
+        assert_equal 1 (count_neighbours (Point 0 0) [(Point 1 1)])
 
